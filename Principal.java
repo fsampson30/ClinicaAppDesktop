122371285c3de1a245e48eed22c951f8774145df
Flavio Sampson <71216861+fsampson30@users.noreply.github.com>
2020-09-13 13:35:11 -0300

    Classe main

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package sonusdesktop;
+
+import View.TelaSenha;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+        TelaSenha telaSenha = new TelaSenha();
+    }
+    
+}
